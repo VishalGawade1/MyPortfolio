03fe5c4d3f7ceb9e482e09f23744fe874c99a8d9
Vishal Gawade <vishalgawade311@gmail.com>
2024-09-23 12:59:42 -0500

    Changing Portfolio Index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Vishal Gawade | Software Engineer Portfolio</title>
+        <meta name="description" content="Software Engineer Portfolio showcasing Vishal Gawade's work and skills in software development" />
+      </Helmet>
       <Main />
     </div>
   );
